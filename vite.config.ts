f102b589d2189cd2dcef92f683613550d13ad50e
porttamcha22 <portniaginevgenii270679@gmail.com>
2026-03-11 20:54:16 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
 export default defineConfig(({ mode }) => ({
-  base: "/port-tamcha-drivers/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
